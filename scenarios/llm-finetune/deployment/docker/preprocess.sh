3b7c0a0e1609c8add2053153dec9c217bc99ede1
Sarang Galada <32020201+saranggalada@users.noreply.github.com>
2025-02-17 11:00:35 +0000

    added llm-finetune scenario

---
+++ scenarios/llm-finetune/deployment/docker/preprocess.sh
@@ -0,0 +1,8 @@
+export DATA_DIR=$PWD/../../data
+export MEDQA_INPUT_PATH=$DATA_DIR/medqa  
+export MEDQA_OUTPUT_PATH=$DATA_DIR/medqa/preprocessed
+export MEDQUAD_INPUT_PATH=$DATA_DIR/medquad
+export MEDQUAD_OUTPUT_PATH=$DATA_DIR/medquad/preprocessed
+export CHATDOCTOR_INPUT_PATH=$DATA_DIR/chatdoctor
+export CHATDOCTOR_OUTPUT_PATH=$DATA_DIR/chatdoctor/preprocessed
+docker compose -f docker-compose-preprocess.yml up --remove-orphans
